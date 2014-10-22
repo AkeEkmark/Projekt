93b25d1d03a8f727732b6a9d19392310cb4cf07d
ufk10ssa <ufk10ssa@09838-STUD-D.HIG-AD.hig.se>
2014-10-22 12:11:15 +0200

    knappar

--- Casino/gui/Gui.java
+++ Casino/gui/Gui.java
@@ -3,12 +3,9 @@ import javax.swing.*;
 
 public class Gui {
 	public Gui(){
-		JFrame f = new JFrame();
-		f.setSize(250,50);
-		f.setVisible(true);
-		f.add(new JToggleButton("difficulty:cakewalk"));
-		f.add(new JToggleButton("difficulty:kinda good"));
-		f.add(new JToggleButton("difficulty:Mr.Smartypants"));
+		StartUpMenu sum = new StartUpMenu();
+
+		 
 	}
 	
 	
--- Casino/gui/StartUpMenu.java
+++ Casino/gui/StartUpMenu.java
@@ -1,7 +1,28 @@
 package gui;
+
 import javax.swing.*;
 
-public class StartUpMenu {
-	
+public class StartUpMenu extends JFrame {
+
+
+	public StartUpMenu() {
+
+
+		setSize(1000, 1000);
+		 JToggleButton d1 = new JToggleButton("Difficulty 1");
+		 JToggleButton d2 = new JToggleButton("Difficulty 2");
+		 JToggleButton d3 = new JToggleButton("Difficulty 3");
+		 d1.setSize(150, 50);
+		 d2.setSize(150, 50);
+		 d3.setSize(150, 50);
+		 d1.setLocation(0, 0);
+		 d2.setLocation(150, 0);
+		 d3.setLocation(300, 0);
+		 add(d1);
+		 add(d2);
+		 add(d3);
+		 setVisible(true);
+		
+	}
 
 }
